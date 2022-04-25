bc427e411c557de12e8901cbed30e075e0e4818d
daixi98 <x3dai@ucsd.edu>
2022-04-25 10:12:33 -0700

    fix bugs

--- public_html/cgi-bin/php-general-request-echo.php
+++ public_html/cgi-bin/php-general-request-echo.php
@@ -4,7 +4,8 @@
     </head>
     <body>
         <h1 align=center>General Request Echo</h1>
-        <hr/>         
+        <hr/>     
+        <?php>    
             echo "<tr> <td>Protocol:</td><td>" . $_SERVER['SERVER_PROTOCOL'] . "</td></tr>";
             echo "<tr> <td>Method:</td><td>" . $_SERVER['REQUEST_METHOD'] . "</td></tr>";
             echo "<tr> <td>Message Body:</td><td></td></tr>";   
