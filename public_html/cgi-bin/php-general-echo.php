bfc41daea890c9db4347913ccf6e2604d5f2abfa
daixi98 <x3dai@ucsd.edu>
2022-04-25 09:54:12 -0700

    general req

---
+++ public_html/cgi-bin/php-general-echo.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Genreal Request Echo</title>
+    </head>
+    <body>
+        <h1 align=center>General Request Echo</h1>
+        <hr/>         
+            echo "<tr> <td>Protocol:</td><td>" . $_SERVER['SERVER_PROTOCOL'] . "</td></tr>";
+            echo "<tr> <td>Method:</td><td>" . $_SERVER['REQUEST_METHOD'] . "</td></tr>";
+            echo "<tr> <td>Message Body:</td><td></td></tr>";   
+        ?>
+       
+        <?php
+            foreach($_POST as $k => $v){
+                echo $k . ": " . $v. "</br>";
+            }
+        ?>
+    </body>
+</html>
