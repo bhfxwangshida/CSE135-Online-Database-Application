abfa22ad41882467a343a5c866c36007f4b51ec5
bhfxwangshida <bhfxwangshida@outlook.com>
2022-05-12 21:25:52 -0700

    eddit get

--- public_html/collector.js
+++ public_html/collector.js
@@ -85,12 +85,4 @@ $(document).ready(function(){
         alert( "error" + "\ncookieID: " + cookieID+ "\nimg_enable: " + img_enable);
         });
     });
-    $("#myButton_get").click(function(){
-        $.get("https://felixwangsd.xyz/api/static/"+get_cookie, function(data, status){
-        alert("Data: " + data + "\nStatus: " + status);
-        })
-        .fail(function(data, status) {
-        alert( "error" + "\nData: " + data + "\nStatus: " + status);
-        });
-    });
 });
